2c0f9aa41319161742613da8091ba8bfc350b268
Hwan <dls5233@yahoo.com>
2021-09-12 17:45:27 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import App from './App'; // import src/App.js
+/* html에서 갱신되는 컴포넌트는 하나의 컴포넌트만 rendering 한다. */
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // index.html id = 'root'
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
