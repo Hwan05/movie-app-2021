e7d356fcad2a6c5a562fa56ba29c8f8a8b536fa0
Hwan <dls5233@yahoo.com>
2021-09-13 20:50:53 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+class App extends React.Component {
+  state = {
+    count : 0
+  };
 
-function Food ({name,picture,rating}){
-  return (
-    <div>
-    <h1> I like {name} </h1>
-    <h4>{rating}/5.0 </h4>
-    <img src = {picture} width = "200" alt={name} />
-    </div>
-  );
-}
+// render stat 값을 setState을 통페이지가 refresh 되고 render function가 다시 호출
+// 직접 state object를 호출하는것은 좋지 않다.
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  }; // 현재 state를 효율적으로 가져오는 방법
+  minus = () => {
+    this.setState({count: this.state.count - 1});
+  };
 
-Food.propTypes  = { // 인가한 프로퍼티 내가 원하는 형태가 맞는지 확인
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired //rating 값이 정수인지
+  render(){
+    return (
+      <div>
 
-};
-
-
-const foodILike = [
-  {
-  id : 1,
-  name : "kimchi",
-  image :
-  "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  rating :5.0
-},
-  {
-  id : 2,
-  name : "Samgyeopsal",
-  image :
-  "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  rating : 4.9
+      <h1> the number is : {this.state.count} </h1>
+      <button onClick={this.add}>Add </button>
+      <button onClick={this.minus}>Minus </button>
 
 
-},
-  {
-  id : 3,
-  name : "Bibibap",
-  image :
-  "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  rating : 3.5
-},
-  {
-  id : 4,
-  name : "Doncasu",
-  image :
-  "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-  rating : 4.3
-}];
+      </div>
+    );
+  }
 
-function renderFood(dish){
-  console.log(dish);
-  return <Food key = {dish.id} name ={dish.name} picture = {dish.image}/>;
-}
-function App() {
-  return (<div> {foodILike.map( dish => (
-    <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating} />))}</div>
-);
 }
 
 export default App;
