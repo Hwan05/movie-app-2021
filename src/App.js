6b1c275766ae614193cdb7de939859d3bee94c0e
Hwan <dls5233@yahoo.com>
2021-09-13 21:27:36 +0900

    Update App.js
    setTimeout(),  boolean,  state

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
+
   state = {
-    count : 0
-  };
+    isLoading: true,
+    movies : []
+  }; // 기본 loading true
 
-// render stat 값을 setState을 통페이지가 refresh 되고 render function가 다시 호출
-// 직접 state object를 호출하는것은 좋지 않다.
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  }; // 현재 state를 효율적으로 가져오는 방법
-  minus = () => {
-    this.setState({count: this.state.count - 1});
-  };
 
-  componentDidMount(){ // Component first render
-    console.log("Component rendered");
+// component first start
+  componentDidMount() {
+    // loading 6초 설정
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+
+
   }
-
-  componentDidUpdate() { // Component update, Rerender
-    console.log("Component update");
-  }
-
-  componentWillUnmount() { // render finish
-    console.log("Goodbye");
-  }
-
-
 
   render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-
-      <h1> the number is : {this.state.count} </h1>
-      <button onClick={this.add}>Add </button>
-      <button onClick={this.minus}>Minus </button>
-
-
+    // this.state object 할당
+    const { isLoading } = this.state;
+    return(
+      <div> {isLoading ? "Loading..." : "We are ready"}
       </div>
     );
   }
-
 }
 
 export default App;
